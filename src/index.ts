45307193235b90cbd1c9ccfa6e5ced01ab20415d
Taylor Lei <taylor.lei@tum.de>
2019-05-16 18:22:28 +0200

    Change runPython to the class RunPython, which is exported as a module

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-const runPython = require('./runPython');
+import RunPython from './RunPython';
 
 const app = express();
 const port = 8080; // default port to listen
@@ -8,10 +8,11 @@ const port = 8080; // default port to listen
 app.get('/', (req, res) => {
   console.log('Someone connected.');
 
-  runPython('./src/demo.py', 'This is a message from index.js').then(
+  const python = new RunPython('./src/demo.py', 'This is a message from index.js');
+  python.getPromise().then(
     (data: string) => {
       console.log(data.toString());
-      res.send('Hello World!' + data.toString());
+      res.send('Hello World! ' + data.toString());
     },
     (error: Error) => {
       console.log(error);
--- src/runPython.ts
+++ src/runPython.ts
@@ -1,16 +1,22 @@
-let runPython = (script: string, arg1: string) => {
-  return new Promise((resolve, reject) => {
-    const { spawn } = require('child_process');
-    const pythonScript = spawn('python', [script, arg1]);
+export default class RunPython {
+  promise: Promise<any>;
 
-    pythonScript.stdout.on('data', (data: string) => {
-      resolve(data);
+  constructor(script: string, arg1: string) {
+    this.promise = new Promise((resolve, reject) => {
+      const { spawn } = require('child_process');
+      const pythonScript = spawn('python', [script, arg1]);
+
+      pythonScript.stdout.on('data', (data: string) => {
+        resolve(data);
+      });
+
+      pythonScript.stderr.on('data', (data: string) => {
+        reject(new Error('Something went wrong! ' + data));
+      });
     });
+  }
 
-    pythonScript.stderr.on('data', (data: string) => {
-      reject(new Error('Something went wrong! ' + data));
-    });
-  });
-};
-
-module.exports = runPython;
+  getPromise() {
+    return this.promise;
+  }
+}
