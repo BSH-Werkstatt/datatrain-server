e7d18c14cd4ec31793aded732debef9466fb97ee
Emil Oldenburg <emil.oldenburg@tum.de>
2019-05-20 15:52:25 -0700

    replaced webpack & added static asset serving

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import RunPython from './runPython';
+import path from 'path';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT ? process.env.PORT : 5000;
+
+app.get('/', express.static(path.join(__dirname, 'site')));
 
 // define a route handler for the default home page
-app.get('/', (req, res) => {
+app.get('/api/python', (req, res) => {
   console.log('Someone connected.');
 
   const python = new RunPython('./src/demo.py', 'This is a message from index.js');
