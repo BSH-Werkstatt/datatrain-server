9f789542bebada1d7f490f567e22f26531dbc8d0
Taylor Lei <taylor.lei@tum.de>
2019-05-29 08:57:44 +0200

    basic structure of the images API endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata'; // this shim is required
 import { createExpressServer } from 'routing-controllers';
 import { MainController } from './controller/main-controller';
+import { ImageController } from './controller/image-controller';
 
 const port = process.env.PORT || 5000;
 
 const app = createExpressServer({
-  controllers: [MainController] // we specify controllers we want to use
+  controllers: [MainController, ImageController] // we specify controllers we want to use
 });
 
 app.listen(port);
