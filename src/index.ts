b4fab10c00d2c67362f595eb3dd4275a1391fffb
parikshit.singh-ext <parikshit.singh-ext@bshg.com>
2020-02-25 15:15:09 +0530

    removed passport support for now

--- src/index.ts
+++ src/index.ts
@@ -15,44 +15,16 @@ import { UserService } from './services/UserService';
 import { User } from './models/user';
 import { CodeStarNotifications } from 'aws-sdk';
 import { preProcessFile, isNewExpression } from 'typescript';
-import passport from 'passport';
-// do not have typescript support @TODO: Add ts support
-const AtlassianCrowdStrategy = require('passport-atlassian-crowd2').Strategy;
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
-/*
-  @TODO: fill the arrrow function
-*/
-passport.serializeUser((user: any, done: any) => {
-  console.log(user);
-  done(null, user);
-});
-passport.deserializeUser((user: any, done: any) => {
-  console.log(user);
-  done(null, user);
-});
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
 });
-passport.use(
-  new AtlassianCrowdStrategy(
-    {
-      crowdServer: process.env.CROWD_URL,
-      crowdApplication: process.env.CROWD_APP,
-      crowdApplicationPassword: process.env.CROWD_PASS,
-      retrieveGroupMemberships: true
-    },
-    (user: any, done: any) => {
-      console.log(user);
-      return done(null, user);
-    }
-  )
-);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/images/:campaignId/:imageId.jpg', (req, res) => {
   if (req.query.userToken) {
@@ -64,15 +36,6 @@ app.get('/images/:campaignId/:imageId.jpg', (req, res) => {
       }
     });
   }
-});
-app.post('/login', passport.authenticate('atlassian-crowd', { failureRedirect: '/fail' }), (req, res) => {
-  res.redirect('/suc');
-});
-app.get('/fail', (req, res) => {
-  res.send('fail');
-});
-app.get('suc', (req, res) => {
-  res.send('suc');
 });
 app.get('/images/:imageId.jpg', (req, res) => {
   if (req.query.userToken) {
